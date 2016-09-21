d93d733e3d6c3f0b35cdbbcbb702c4c04bf78a79
LeMon <sakchai.ha@gmail.com>
2016-09-21 14:22:42 +0700

    code From

--- app/src/main/java/sut/lemon/sakchai/sutfriend/SignUpActivity.java
+++ app/src/main/java/sut/lemon/sakchai/sutfriend/SignUpActivity.java
@@ -1,9 +1,13 @@
 package sut.lemon.sakchai.sutfriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -14,8 +18,11 @@ public class SignUpActivity extends AppCompatActivity {
             phoneEditText,
             userEditText,
             passwordEditText;
-    private String nameString,ardressString,phoneString,userString,
+    private String nameString,addressString,phoneString,userString,
             passwordString,genderString,imageString;
+    private RadioButton maleRadioButton, femaleRadioButton;
+    private ImageView imageView;
+
 
 
     @Override
@@ -29,17 +36,55 @@ public class SignUpActivity extends AppCompatActivity {
         phoneEditText = (EditText) findViewById(R.id.editText3);
         userEditText = (EditText) findViewById(R.id.editText4);
         passwordEditText = (EditText) findViewById(R.id.editText5);
+        maleRadioButton = (RadioButton) findViewById(R.id.radioButton);
+        femaleRadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        //ImagesView Controler
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("images/*");
+                startActivityForResult(Intent.createChooser(intent, "โปรดเลือกรูปภาพ"), 1);
+            }// onclick
+        });
 
     }//Main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode == 1) && (requestCode == RESULT_OK)) {
+
+            Log.d("sutFriendV1", "Result ==> Success");
+
+        }//if
+    }
 
     public void clickSignUpSign(View view) {
 
         //GET VALUE From Edit Text
         nameString = nameEditText.getText().toString().trim();
-        ardressString = addressEditText.getText().toString().trim();
+        addressString = addressEditText.getText().toString().trim();
         phoneString = phoneEditText.getText().toString().trim();
         userString = userEditText.getText().toString().trim();
         passwordString = passwordEditText.getText().toString().trim();
+
+        //check Space
+        if (nameString.equals("") || addressString.equals("") ||
+                phoneString.equals("") || userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert(this, R.drawable.doremon48,
+                    "มีช่องว่าง", "กรุณากรอกทุกช่อง ครับ");
+            myAlert.myDialog();
+        } else if (!(maleRadioButton.isChecked() || femaleRadioButton.isChecked())) {
+            //Non Check
+            MyAlert myAlert = new MyAlert(this, R.drawable.nobita48,
+                    "ยังไม่เลือก Gender", "กรุณาเลือก Gender");
+            myAlert.myDialog();
+        }
 
     }//clicksignUp
 
